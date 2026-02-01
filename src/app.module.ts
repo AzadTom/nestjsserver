dc290622071b98c87f9d52a13ad0cbafc0fdf49a
azadtom <kumarazad2917@gmail.com>
2026-02-01 11:19:10 +0530

    comment out typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,22 +14,22 @@ import { TenantModule } from './tenant/tenant.module';
       isGlobal: true,
       envFilePath: '.env'
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: "postgres",
-        host: config.get("DB_HOST"),
-        port: config.get<number>('DB_PORT'),
-        username: config.get('DB_USER'),
-        password: config.get('DB_PASS'),
-        database: config.get('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-        logging: true,
-      })
-    }),
-    AuthModule, UserModule,MailModule, TenantModule],
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService) => ({
+    //     type: "postgres",
+    //     host: config.get("DB_HOST"),
+    //     port: config.get<number>('DB_PORT'),
+    //     username: config.get('DB_USER'),
+    //     password: config.get('DB_PASS'),
+    //     database: config.get('DB_NAME'),
+    //     autoLoadEntities: true,
+    //     synchronize: true,
+    //     logging: true,
+    //   })
+    // }),
+    AuthModule, UserModule, MailModule, TenantModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { join } from 'node:path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 
@@ -8,9 +7,6 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
